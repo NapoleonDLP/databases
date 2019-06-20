dce9eab0bcca06d4063b8e811bff48b3adea2864
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-20 16:59:16 -0700

    Receives request and makes async call to models

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,38 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) { select users.name, rooms.name, messages.message from messages inner join rooms on rooms.id = messages.room_id inner join users on users.id = messages.user_id}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      //handle req dispatch to models => models => database
+
+      //pass in req body = message coming from client?
+      //messages {
+      //username
+      //roomname
+      //message
+      //}
+      console.log('****************');
+      console.log(req);
+
+      models.messages.get('/messages', (err, messages) =>{
+
+        //checks if there is an error coming from models
+        if (err) {
+          //sends error code
+          res.send(err);
+        } else {
+          //if messages comes back are written in response
+          res.send(messages);
+          //res.json(messages)
+          // res.status()
+
+        }
+      });
+      // }
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
+
+
 
   users: {
     // Ditto as above
