40cf650707152cd17d0ddbf7179fe781d1a8ffa1
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-20 16:57:39 -0700

    makes async query to DB

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,48 @@
 var db = require('../db');
+var mysql = require('mysql');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (route, callback) {
+
+      //query database
+
+      // let conStr = 'select users.name, rooms.name, messages.message from messages inner join rooms on rooms.id = messages.room_id inner join users on users.id = messages.user_id';
+
+      let conStr = 'select messages.message from messages';
+      console.log('**************');
+      console.log(conStr);
+      //our problem is here. It does not recognize the .query
+
+      //error code was not recognizing db.connection.query()
+
+      db.query (conStr, function(err, result) {
+        if (err) {
+          callback(err, null);
+        } else {
+          //result coming back as an empty array
+          console.log('******result*****');
+          console.log(result);
+          callback(null, result);
+        }
+      });
+    },
     post: function () {} // a function which can be used to insert a message into the database
-  },
+  }, // a function which produces all the messages
+
+
+  // },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
-  }
+    get: function () {
+    // let conStr = `SELECT EXISTS( SELECT 1 FROM users WHERE id = ${id})`;
+    // db.query(conStr, function(error, result, field) {
+    //   if (error) throw error;
+      //  console.log(result[conStr]); //--> 1
+      //});
+    }
+  },
+  post: function () {}
 };
 
