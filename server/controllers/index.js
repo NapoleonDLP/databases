cf9d1dc4e10a04597cd84f0a199f8c57ab976318
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-20 18:55:08 -0700

    cleans up comments

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,38 +3,24 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
-      //handle req dispatch to models => models => database
-
-      // console.log('****************');
-      // console.log(req);
 
       models.messages.get('/messages', (err, messages) => {
-
-        //checks if there is an error coming from models
         if (err) {
-          //sends error code
           res.send(err);
         } else {
-          //if messages comes back are written in response
           res.send(messages);
-          //res.json(messages)
-          // res.status()
-
         }
       });
-    }, // a function which handles a get request for all messages
+    },
 
     post: function (req, res) {
-      // if (err) {
-      //   res.send();
-      // } else {
+
       let message = {
         username: '1',
         roomname: '1',
         message: 'This is a test from mock data'
       };
-      // console.log('*****MESSAGE*******');
-      // console.log(req.body);
+
       models.messages.post('/messages', message, (err, messages) => {
         if (err) {
           res.send(err);
@@ -42,15 +28,16 @@ module.exports = {
           res.send(messages);
         }
       });
-      // }
-    } // a function which handles posting a message to the database
+    }
   },
 
 
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
+    get: function (req, res) {
+
+    },
     post: function (req, res) {}
   }
 };
