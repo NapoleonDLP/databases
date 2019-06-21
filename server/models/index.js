c9ea2ec22240005989fd5b49cadd2a0edb3aae81
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-20 17:39:24 -0700

    post db.query and writes to db

--- server/models/index.js
+++ server/models/index.js
@@ -7,11 +7,11 @@ module.exports = {
 
       //query database
 
-      // let conStr = 'select users.name, rooms.name, messages.message from messages inner join rooms on rooms.id = messages.room_id inner join users on users.id = messages.user_id';
+      // let conStr = 'select users.name, rooms.name, messages.message \ from messages inner join rooms on rooms.id = messages.room_id \ inner join users on users.id = messages.user_id';
 
       let conStr = 'select messages.message from messages';
-      console.log('**************');
-      console.log(conStr);
+      // console.log('**************');
+      // console.log(conStr);
       //our problem is here. It does not recognize the .query
 
       //error code was not recognizing db.connection.query()
@@ -21,13 +21,29 @@ module.exports = {
           callback(err, null);
         } else {
           //result coming back as an empty array
+          // console.log('******result*****');
+          // console.log(result);
+          callback(null, result);
+        }
+      });
+    },
+    post: function (route, params, callback) {
+      console.log('**************');
+      console.log(params);
+      let conStr = 'insert into messages (user_id ,room_id, message) VALUES (?,?,?)';
+
+      db.query (conStr, [params.username, params.roomname, params.message], function(err, result) {
+        if (err) {
+          callback(err, null);
+        } else {
+          //result coming back as an empty array
           console.log('******result*****');
           console.log(result);
           callback(null, result);
         }
       });
-    },
-    post: function () {} // a function which can be used to insert a message into the database
+
+    } // a function which can be used to insert a message into the database
   }, // a function which produces all the messages
 
 
