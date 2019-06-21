46bf7af3c8ef68da1402436348ba11e12ac32771
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-20 19:21:54 -0700

    Adjusts to make post to DB from request

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -15,12 +15,7 @@ module.exports = {
 
     post: function (req, res) {
 
-      let message = {
-        username: '1',
-        roomname: '1',
-        message: 'This is a test from mock data'
-      };
-
+      let message = req.body;
       models.messages.post('/messages', message, (err, messages) => {
         if (err) {
           res.send(err);
@@ -36,9 +31,29 @@ module.exports = {
   users: {
     // Ditto as above
     get: function (req, res) {
-
+      models.users.get('/users', (err, users) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(users);
+        }
+      });
     },
-    post: function (req, res) {}
+    post: function (req, res) {
+      let message = {
+        username: '2',
+        roomname: '2',
+        message: 'This is a test from mock data'
+      };
+
+      models.users.post('/users', username, (err, users) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(/* messages  */);
+        }
+      });
+    }
   }
 };
 
